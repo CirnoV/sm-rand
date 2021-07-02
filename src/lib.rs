ae6c84f77b8937e557e001451030981df01f7c5b
CirnoV <cirnov59@gmail.com>
2021-07-02 17:40:52 +0900

    Fix panic when nmin larger then nmax

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,20 @@ use rand::Rng;
 
 #[no_mangle]
 pub extern "C" fn get_random_int(nmin: i32, nmax: i32) -> i32 {
+    let mut nmin = nmin;
+    let mut nmax = nmax;
+    if nmin > nmax {
+        std::mem::swap(&mut nmin, &mut nmax);
+    }
     rand::thread_rng().gen_range(nmin..=nmax)
 }
 
 #[no_mangle]
 pub extern "C" fn get_random_float(nmin: f32, nmax: f32) -> f32 {
+    let mut nmin = nmin;
+    let mut nmax = nmax;
+    if nmin > nmax {
+        std::mem::swap(&mut nmin, &mut nmax);
+    }
     rand::thread_rng().gen_range(nmin..=nmax)
 }
